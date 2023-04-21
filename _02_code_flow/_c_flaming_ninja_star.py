c8b2cad2b65bffc0724e9801145dd0c17e1ed7b4
League <zach.dantsker@gmail.com>
2023-04-21 16:55:27 -0700

    hi

--- _02_code_flow/_c_flaming_ninja_star.py
+++ _02_code_flow/_c_flaming_ninja_star.py
@@ -21,49 +21,49 @@ if __name__ == '__main__':
     flameSize = 130         # the length of the flaming arms
     
     # Make a new turtle
-    
+    bob = turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    
+    bob.shape('turtle')
     # Set the turtle width to 2
-    
+    bob.width(2)
     # Set the turtle speed to 0 (fastest)
-    
+    bob.speed(0)
     # Use a for loop to repeat all of the code below ONE time (we will change
     # this later)
-        
+    for i in range(25):
         # Set the turtle .fillcolor() to orange
-        
+        bob.fillcolor('orange')
         # Call the turtle .begin_fill() function
-        
+        bob.begin_fill()
         # TURN RIGHT     Turn the turtle 1/8 of a circle (hint: 360 degrees
         #                will turn a full circle)
-        
+        bob.right(360/7)
         # DRAW           Move the turtle 64 pixels
-        
+        bob.forward(64)
         # TURN LEFT      Turn the turtle 40 degrees to the LEFT. (Negative
         #                numbers will turn the turtle counter-clockwise.)
-        
+        bob.left(40)
         # DRAW FLAME     Move the turtle the distance in the variable flameSize
-        
+        bob.forward(flameSize)
         #                Turn the turtle to the right 170 degrees
-         
+        bob.right(170)
         #                Move the turtle the distance in the variable flameSize (again)
-         
+        bob.forward(flameSize)
         #  TURN RIGHT    Turn the turtle 62 degrees to the right
-        
+        bob.right(62)
         #  DRAW          Move the turtle the distance in the variable baseSize
-        
+        bob.forward(baseSize)
         # Call the turtle .end_fill() method
-        
+        bob.end_fill()
     # Hide your turtle so you can see the pattern.
-        
+    bob.hideturtle()
     # TEST   Run the program. Check that your shape is the same as the first
     #        picture in the recipe. This is one arm of the ninja star.
 
     # COLOR  Change the turtle's pen color so that the flame is a different
     #        color to the rest of the star. Run the program again. Check the
     #        second picture in the recipe.
-
+    bob.pencolor('red')
     # LOOP   When you have one arm looking right, change your for loop to
     #        repeat 25 times.
     
--- _03_print_and_popups/_e_awesome_or_not.py
+++ _03_print_and_popups/_e_awesome_or_not.py
@@ -2,17 +2,29 @@ from tkinter import messagebox, simpledialog, Tk
 import random
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
+if __name__ == '__main__':
 
+    # Make a new window variable, window = Tk()
+    window = Tk()
+    # Hide the window using the window's .withdraw() method
+    window.withdraw()
     # 1. Make a variable equal to a positive number less than 4 using random.randInt(0, 3)
-    
+    number = random.randint(0,3)
     # 2. Print your variable to the console
-    
+    print(number)
     # 3. Get the user to enter something that they think is awesome
-    
+    awsome = simpledialog.askstring(None, prompt="what do you think is awsome?")
+    if awsome == "prime":
+        messagebox.showinfo(None, message="PRIME IS THE BEST DRINK EVER!!!!!!!!!!!!")
+    else:
+        if number == 0:
+            messagebox.showinfo(None, message="that is awsome")
+        if number == 1:
+            messagebox.showinfo(None, message="oh, thats ok")
+        if number == 2:
+            messagebox.showinfo(None, message="that is boring")
+        if number == 3:
+            messagebox.showinfo(None, message="BRO THAT IS THE LAMEST THING THAT EVER EXISTED THAT IS SOOOO BAD!!!!!!!!")
     # 4. If your variable is  0
         # -- tell the user whatever they entered is awesome!
         
@@ -26,3 +38,4 @@ import random
         # -- invent your own message to give to the user (be nice).
         
     # Run the window's .mainloop() method
+    window.mainloop()
